125a0be377247af2a736b668a72204eb2bee7aa3
Noah S Boussios <boussiosn28144@student.cghsnc.org>
2025-05-21 16:29:22 -0400

    Version 1.0

--- framework.py
+++ framework.py
@@ -1,3 +1,4 @@
+import time
 inventory = []
 number_endings = ["st", "nd", "rd", "th"]
 
@@ -10,9 +11,12 @@ class Item:
         if self.owned == False:
             self.owned = True
             inventory.append(self)
+            
             print(f"You picked up {self.name}.")
+            time.sleep(2)
         else:
             print(f"You already have {self.name}.")
+            time.sleep(1)
     def use(self, target):
         pass
 
@@ -24,13 +28,17 @@ class Puzzle:
 class Padlock(Puzzle):
     def run(self):
         print("You see a padlock. It has a 3-digit combination.")
+        time.sleep(4)
         print("Enter the combination:")
+
         user_input = input()
         if user_input == self.answer:
             print("You hear a click and with a pull, the padlock clicks open!")
+            time.sleep(2)
             return True
         else:
             print("You try to pull the padlock open, but nothing budges.")
+            time.sleep(2)
             return False
         
 class Keyhole(Puzzle):
@@ -39,15 +47,21 @@ class Keyhole(Puzzle):
         self.key = key
     def run(self):
         print("You see a keyhole. It takes in a specific key.")
+        time.sleep(3)
         resp = input(("Which Item Will you Like to Use?: "+", ".join([item.name for item in inventory])))
         for item in inventory:
             if item.name == resp:
+                time.sleep(2)
                 print(f"You try to insert the {item.name} into the keyhole.")
                 if item.name == self.key.name:
+                    time.sleep(2)
                     print("The key fits perfectly! With a pull, the keyhole clicks open!")
+                    time.sleep(1)
                     return True
                 else: 
+                    time.sleep(2)
                     print("The key doesn't fit. You try to pull the keyhole open, but nothing budges.")
+                    time.sleep(1)
                     return False
 
 class Switches(Puzzle):
@@ -57,24 +71,32 @@ class Switches(Puzzle):
         self.num_switches = num_switches
     def run(self):
         directions = []
+        time.sleep(2)
         print(f"You see a series of {self.num_switches} switches. Each switch can be either up or down.")
+        time.sleep(2)
         i = 0
         end = number_endings[0]
-        if i >-1 and i <=2:
-            end = number_endings[i]
-        else:
-            end = number_endings[3]
         while i < self.num_switches: 
-            us = input(f"Enter the {str(i+1)+number_endings[end]} switches direction(up/down):")
+            if i >-1 and i <=2:
+                end = number_endings[i]
+            else:
+                end = number_endings[3]
+            us = input(f"Enter the {str(i+1)+end} switches direction(up/down):")
             if us.lower() == "up" or us.lower() == "down":
                 directions.append(us.lower())
+                i+=1
+                time.sleep(1)
+                print("You flipped the switch "+us.lower()+".")
+                time.sleep(2)
             else:
                 print("Invalid input. Please enter 'up' or 'down'.")
-                i -= 1
+                time.sleep(2)
         if self.answer == directions:
+            time.sleep(2)
             print("You feel a small pull on the last switch, and with a pull, a secret door opens exposing a golden key!")
             return True
         else:
+            time.sleep(2)
             print("You try to pull the switches, but nothing happens.")
             return False
             
@@ -102,26 +124,33 @@ class Area:
     def solve_puzzle(self, puzzle):
         answer = puzzle.run()
         if answer == True:
-            print("You solved the puzzle!")
+            time.sleep(2)
             self.solved = True
-            print(f"You receive {self.reward.name} as a reward.")
+            time.sleep(2)
+            print(f"{self.reward.name} is now available in your inventory.")
             inventory.append(self.reward)
-            self.remove_item(self.reward)
         else:
+            time.sleep(2)
             print("The puzzle remains unsolved.")
     def inspect(self):
         print(f"You inspect the {self.name}.")
+        time.sleep(2)
         choice = input("Would you like to do? (1) inspect inventory (2) solve puzzle (3) quit inspect: ")
         if choice == "1":
+            time.sleep(1)
             print("Items in your inventory:")
             for item in inventory:
-                print(f"- {item.name}: {item.description}")
+                time.sleep(.2)
+                print(f"- {item.name}")
+            time.sleep(1)
             item_name = input("Which item would you like to inspect?")
             for item in inventory:
                 if item.name == item_name:
+                    time.sleep(1)
                     print(f"{item.name}: {item.description}")
                     break
             else:
+                time.sleep(1)
                 print("Item not found in inventory.")
             self.inspect()
         elif choice == "2":
@@ -130,48 +159,70 @@ class Area:
                     self.solve_puzzle(puzzle)
                     if self.solved == True:
                         self.puzzles.remove(puzzle)
+                        time.sleep(1)
                         print(f"You solved the {puzzle.name} puzzle!")
             else:
+                time.sleep(1)
                 print("There are no puzzles here.")
             self.inspect()
         elif choice == "3":
+            time.sleep(2)
             print("You quit inspecting the area.")
+            
         else:
             print("Invalid choice.")
             self.inspect()
 class Game:
     def __init__(self, areas):
         self.areas = areas
+        self.is_playing = True
     def main(self):
+        time.sleep(2)
         print("What would you like to do? (1) Inspect area (2) Use item (3) View Candle")
         ans = input()
         if ans == "1":
             self.cd()
         elif ans == "2":
+            time.sleep(1)
             print("Items in your inventory:")
             for item in inventory:
-                print(f"- {item.name}: {item.description}")
+                time.sleep(.2)
+                print(f"- {item.name}")
+            time.sleep(1)
             item_name = input("Enter the name of the item you want to use: ")
             for item in inventory:
                 if item.name == item_name:
-                    print(f"You used {item.name}.")
+                    time.sleep(1)
                     print(item.name + ": " + item.description)
-                    break
-            else:
-                print("Item not found in inventory.")
+                if item.name.lower() == "reality":
+                    time.sleep(7)
+                    print("What.?.?")
+                    time.sleep(2)
+                    print("You snap back to reality and realize you never tried to open the coffin.")
+                    time.sleep(2)
+                    print("With just a little bit of force, you kick the lid off and find yourself in the middle of your living room.")
+                    time.sleep(4)
+                    print("You're confused, but that doesn't matter right now. You're safe!")
+                    self.is_playing = False
+                    return
+            
             self.main()
         elif ans == "3":
+            time.sleep(1)
             print("You see a candle. It is lit and provides light.")
             self.main()
         else:
+            time.sleep(1)
             print("Invalid choice.")
             self.main()
     def cd(self):
         areas = self.areas
+        time.sleep(2)
         answer = input("There are a few visible objects around you: "+", ".join([area.name for area in areas])+"\n"+"Enter location you Would Like to Inspect: ").lower()
-
+        time.sleep(1)
         area = next((area for area in areas if area.name.lower() == answer), None)
         if area:
             area.inspect()
         else:
+            time.sleep(1)
             print("Invalid location.")
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import framework
-silver_key = framework.Item("Silver Key",False, "A shiny silver key that fits the silver keyhole.")
+import time
 
+silver_key = framework.Item("Silver Key",False, "A shiny silver key that fits the silver keyhole.")
+gold_key = framework.Item("Gold Key",False, "A shiny gold key that fits the gold keyhole.")
+note = framework.Item("Note", False, "A note that reads: The first, is sometimes also the best. Undead Demons Utter Unholy Desires.")
 padlock = framework.Padlock("Padlock", "A small padlock with a 3-digit combination.", "547")
 padlock_box = framework.Area("Padlock Box", "A small box with a padlock on it.",[padlock], silver_key )
+reality = framework.Item("Reality", False, "A note that reads: If your're reading this, you are probably scared for your life. \n Just know, you're the one that said you wouldn't drink too much.\n Just kick the lid off, no one ever locked it.")
 
+silver_keyhole = framework.Keyhole("Silver Keyhole", "A keyhole that takes in a silver key.", "Silver key", silver_key)
+silver_box = framework.Area("Silver Box", "A small box with a silver keyhole on it.", [silver_keyhole], note)
 
-silver_keyhole = framework.Keyhole("Silver Keyhole", "A keyhole that takes in a silver key.", "Silver key", "Silver Key")
-silver_box = framework.Area("Silver Box", "A small box with a silver keyhole on it.", [silver_keyhole], [silver_key])
-
-gold_keyhole = framework.Keyhole("Gold Keyhole", "A keyhole that takes in a gold key.", "Gold Key", "Gold Key")
-gold_key = framework.Item("Gold Key",False, "A shiny gold key that fits the gold keyhole.")
-gold_box = framework.Area("Gold Box", "A small box with a gold keyhole on it.", [gold_keyhole], [gold_key])
+gold_keyhole = framework.Keyhole("Gold Keyhole", "A keyhole that takes in a gold key.", "Gold Key", gold_key)
+gold_box = framework.Area("Gold Box", "A small box with a gold keyhole on it.", [gold_keyhole], reality )
 
 switches_puzzle = framework.Switches("Switches Puzzle", "A series of switches that can be either up or down.", 5, ["up", "down", "up","up", "down"])
 
@@ -18,17 +20,29 @@ switches = framework.Area("Switches", "A series of switches that can be either u
 
 areas = [padlock_box, gold_box, silver_box, switches]
 
+
 game = framework.Game(areas)
 print("You wake up after a 'great' night, laying in complete darkness.")
+time.sleep(3)
 print("You feel around and realize your in a very small box just bigger than the length of your body.")
+time.sleep(3)
 print("You feel something that feels like a candle to your right.")
+time.sleep(3)
 print("You have a lighter in your pocket from the night before.")
+time.sleep(3)
 print("you light the candle and it all becomes clear.")
-print("YoU ARE IN A COFFIN!")
+time.sleep(3)
+print("YOU ARE IN A COFFIN!")
+time.sleep(3)
 print("You see a number scratched into the wood of the coffin.")
+time.sleep(3)
 print("It reads: 547")
+time.sleep(2)
 print("those numbers are now available in your inventory.")
+time.sleep(3)
 numbers = framework.Item("Numbers", False, "The numbers 547 scratched into the wood of the coffin.")
 framework.inventory.append(numbers)
-game.main()
-
+is_playing = True
+while is_playing:
+    game.main()
+    is_playing = game.is_playing
